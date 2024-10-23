4ecbebcd0b536ce426dde01d353706b3504070b1
Josiah Smith <jcsmithf22@gmail.com>
2024-10-22 20:44:54 -0500

    only load chartkick on relevant page

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,6 +4,7 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin "chartkick", to: "chartkick.js"
-pin "Chart.bundle", to: "Chart.bundle.js"
+pin "chartkick", to: "chartkick.js", preload: false
+pin "Chart.bundle", to: "Chart.bundle.js", preload: false
+pin "insights", to: "insights.js", preload: false
 pin_all_from "app/javascript/controllers", under: "controllers"
