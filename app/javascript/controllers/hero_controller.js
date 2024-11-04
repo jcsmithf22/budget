6e4dfeedba0ef7f69fdb49bbe563021d03bb21de
Josiah Smith <jcsmithf22@gmail.com>
2024-11-04 17:06:36 -0600

    use javascript for animation

--- app/javascript/controllers/hero_controller.js
+++ app/javascript/controllers/hero_controller.js
@@ -1,5 +1,6 @@
 import { Controller } from "@hotwired/stimulus";
 import { useIntersection } from "stimulus-use";
+import { animate } from "motion";
 
 // Connects to data-controller="hero"
 export default class extends Controller {
@@ -11,12 +12,30 @@ export default class extends Controller {
   disappear(entry, observer) {
     document.documentElement.dataset.showHeader = "true";
     // this.element.style.opacity = 0;
-    this.element.classList.add("hide");
+    // this.element.classList.add("hide");
+    animate(
+      this.element,
+      {
+        opacity: 0,
+      },
+      {
+        duration: 0.15,
+      },
+    );
   }
 
   appear(entry, observer) {
     document.documentElement.dataset.showHeader = "false";
     // this.element.style.opacity = 1;
-    this.element.classList.remove("hide");
+    // this.element.classList.remove("hide");
+    animate(
+      this.element,
+      {
+        opacity: 1,
+      },
+      {
+        duration: 0.15,
+      },
+    );
   }
 }
