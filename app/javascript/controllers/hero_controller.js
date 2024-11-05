a645fb2d01ac9a633f2cb52bcab95926c547f7b3
Josiah Smith <jcsmithf22@gmail.com>
2024-11-04 18:15:43 -0600

    try css animation (keyframes)

--- app/javascript/controllers/hero_controller.js
+++ app/javascript/controllers/hero_controller.js
@@ -5,37 +5,40 @@ import { animate } from "motion";
 // Connects to data-controller="hero"
 export default class extends Controller {
   connect() {
-    useIntersection(this, { rootMargin: "-60px 0px 0px 0px" });
+    useIntersection(this, { rootMargin: "-120px 0px 0px 0px" });
     document.documentElement.dataset.showHeader = "false";
   }
 
   disappear(entry, observer) {
     document.documentElement.dataset.showHeader = "true";
     // this.element.style.opacity = 0;
-    // this.element.classList.add("hide");
-    animate(
-      this.element,
-      {
-        opacity: 0,
-      },
-      {
-        duration: 0.15,
-      },
-    );
+    this.element.classList.add("fade-out");
+    this.element.classList.remove("fade-in");
+    // animate(
+    //   this.element,
+    //   {
+    //     opacity: 0,
+    //   },
+    //   {
+    //     duration: 0.15,
+    //   },
+    // );
   }
 
   appear(entry, observer) {
     document.documentElement.dataset.showHeader = "false";
     // this.element.style.opacity = 1;
     // this.element.classList.remove("hide");
-    animate(
-      this.element,
-      {
-        opacity: 1,
-      },
-      {
-        duration: 0.15,
-      },
-    );
+    this.element.classList.remove("fade-out");
+    this.element.classList.add("fade-in");
+    // animate(
+    //   this.element,
+    //   {
+    //     opacity: 1,
+    //   },
+    //   {
+    //     duration: 0.15,
+    //   },
+    // );
   }
 }
